196a0fc16d90279c1d7e38ff3959d772703bae48
rahaf20-meet <rahaf20@meet.mit.edu>
2018-10-17 12:23:16 -0400

    my first commit

---
+++ lab1.py
@@ -0,0 +1,26 @@
+"""
+for i in range (2):
+	print("Rahaf Zorba")
+
+number1 = 4
+print(number1)
+number2 = 2
+print(number2)
+
+num = [3,2,1]
+for i in num:
+	print(i)
+
+range = 
+for i in range:
+	print(i*2)
+"""
+import turtle
+for circle in range():
+	turtle.pensize(25)
+	turtle.color("blue")
+	turtle.penup()
+	turtle.goto(-110,-25)
+	turtle.pendown()
+	turtle.circle(100)
+turtle.mainloop()
---
+++ lab2.py
@@ -0,0 +1,21 @@
+
+def fun(n):
+	a=n[0]
+	b=n[-1]
+	list1=[a,b]
+	return list1 
+
+list2=[2,3,4,5]
+fun(list2)
+n=fun(list2)
+print(n)
+
+def fun2(x):
+	for i in x:
+		if i < 5:
+		 print (i)
+fun2(list2)
+x=fun2(list2)
+print(x)
+
+
