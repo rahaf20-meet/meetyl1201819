d64fd4287e28c1c29b3301607d41e96ff4e5bd12
rahaf20-meet <rahaf20@meet.mit.edu>
2018-10-24 12:24:40 -0400

    whatever

---
+++ lab3.py
@@ -0,0 +1,21 @@
+import turtle
+"""
+turtle.addshape("tire.gif")
+turtle.goto(0,0)
+turtle.shape("tire.gif")
+"""
+for i  in range(3000):
+	turtle.speed(0)
+	turtle.forward(200)
+
+	turtle.left(300)
+	turtle.forward(75)
+	turtle.right(100)
+
+	turtle.forward(20)
+	turtle.right(30)
+	turtle.penup()
+	turtle.home()
+	turtle.pendown()
+	turtle.left(i+30)
+turtle.mainloop()
---
+++ turtleshapes.py
@@ -0,0 +1,22 @@
+import turtle
+for i in range (5):
+	turtle.forward(100)
+	turtle.right(145)
+turtle.penup()
+turtle.left(5)
+turtle.goto(200,200)
+turtle.begin_fill()
+turtle.pendown()
+turtle.forward(170)
+turtle.right(90)
+turtle.forward(125)
+turtle.right(45)
+turtle.forward(120)
+turtle.right(90)
+turtle.forward(120)
+turtle.right(45)
+turtle.forward(130)
+turtle.end_fill()
+
+
+turtle.mainloop()
